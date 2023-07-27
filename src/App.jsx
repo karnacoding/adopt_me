74169abf2065a2c03fad605debd2b9f79f46322e
Kundan <kundankarn007@gmail.com>
2023-07-27 18:05:12 +0530

    react for fun

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { createRoot } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  const adoptedPet = useState(null);
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <header>
+            <Link to="/">Adopt me</Link>
+          </header>
+
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </AdoptedPetContext.Provider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
---
+++ src/SearchParams.jsx
@@ -0,0 +1,84 @@
+import { useState, useContext } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
+import Results from "./Results";
+import useBreedList from "./useBreedList";
+import { useQuery } from "@tanstack/react-query";
+import fetchSearch from "./fetchSearch";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  // add at top of SearchParams render function
+  const [adoptedPet] = useContext(AdoptedPetContext);
+  const [requestParams, setRequestParams] = useState({
+    location: "",
+    animal: "",
+    breed: "",
+  });
+
+  const [animal, setAnimal] = useState("");
+  const [breeds] = useBreedList(animal);
+
+  const results = useQuery(["search", requestParams], fetchSearch);
+  const pets = results?.data?.pets ?? [];
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const formData = new FormData(e.target);
+          const obj = {
+            animal: formData.get("animal") ?? "",
+            breed: formData.get("breed") ?? "",
+            location: formData.get("location") ?? "",
+          };
+          setRequestParams(obj);
+        }}
+      >
+        {adoptedPet ? (
+          <div className="pet image-container">
+            <img src={adoptedPet.images[0]} alt={adoptedPet.name} />
+          </div>
+        ) : null}
+        <label htmlFor="location">
+          Location
+          <input id="location" placeholder="Location" name="location" />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            placeholder="Animal"
+            name="animal"
+            onChange={(e) => setAnimal(e.target.value)}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select disabled={!breeds.length} id="breed" name="breed">
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
